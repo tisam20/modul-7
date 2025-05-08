e0930fcb74031c544b22af3e057976bbace99e68
tisam20 <ibtisam0604@gmail.com>
2025-05-08 16:29:17 +0700

    Add files via upload

---
+++ post test 2/src/Main.java
@@ -0,0 +1,18 @@
+class Orang {
+    Orang() {
+        System.out.println("Orang dibuat!");
+    }
+}
+
+class Dosen extends Orang {
+    Dosen() {
+        super();
+        System.out.println("Dosen dibuat!");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Dosen d = new Dosen();
+    }
+}
